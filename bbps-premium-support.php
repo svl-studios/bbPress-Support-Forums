640b45481370dc73516e169779c8240041eb23c3
dtbaker <dtbaker@gmail.com>
2014-03-18 12:41:03 +1000

    bumb version to 3.0.3

--- bbps-premium-support.php
+++ bbps-premium-support.php
@@ -3,8 +3,8 @@
 Plugin Name: GetShopped Support Forum Plugin
 Plugin URI: http://getshopped.org
 Description: Turn your new bb-Press 2.0 forums into support forums
-Author: Mychelle, GetShopped, mufasa
-Version: 3.0.2
+Author: dtbaker, Mychelle, GetShopped, mufasa
+Version: 3.0.3
 */
 
 //////
